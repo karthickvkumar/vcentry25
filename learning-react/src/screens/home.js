9f729dbc9fba7cbe9abc3e2778a17c74d7eed6fa
kvelraj_concord <kvelraj@concord.net>
2023-04-08 15:28:22 +0530

    source code

--- learning-react/src/screens/home.js
+++ learning-react/src/screens/home.js
@@ -1,9 +1,26 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 const HomeScreen = () => {
   return(
     <div>
-      <h1>Home Screen</h1>
+      <div className="header">
+        <div className="logo">
+
+        </div>
+        <div className="screens-link">
+          <NavLink to="/home" className="link">Home</NavLink>
+          <NavLink to="/about" className="link">About</NavLink>
+          <NavLink to="/contact-us" className="link">Contact</NavLink>
+          <NavLink to="/settings" className="link">Settings</NavLink>
+        </div>
+      </div>
+      <div className="content">
+        <h1>Home Screen</h1>
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   )
 }
