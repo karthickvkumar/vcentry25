93d67afa06e9ae698826d078afb0625cbb38fb5d
kvelraj_concord <kvelraj@concord.net>
2023-04-23 16:19:13 +0530

    source code

--- learning-react/src/screens/about.js
+++ learning-react/src/screens/about.js
@@ -6,11 +6,15 @@ import FooterComponent from "../components/footer";
 const AboutScreen = () => {
 
   //step 2: To utlize the useState function into Coding
-  //const [variableName, functionName] = useState(initialValue);
+  //const [stateVariableName, stateFunctionName] = useState(initialValue);
   const [dynamicValue, updateDynamicValue] = useState("");
 
+  //Step 4: To capture a EVENT, so we are using varailable event
   const triggerTextbox = (event) => {
     console.log(event.target.value);
+    //step 5: To assign value into stateVariableName 
+    // stateFunctionName(value);
+    updateDynamicValue(event.target.value);
   }
 
   return(
@@ -20,8 +24,10 @@ const AboutScreen = () => {
         <h1>This is a About Screen</h1>
         <div className="align-vertical-center">
           <label className="label">Enter your message</label>
+          {/* Step 3: Adding onChange event to Text area and Function */}
           <textarea className="textbox" onChange={triggerTextbox}></textarea>
         </div>
+        <h1>{dynamicValue}</h1>
       </div>
       <FooterComponent></FooterComponent>
     </div>
