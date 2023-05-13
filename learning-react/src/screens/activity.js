21b2ec8432eb08e303cbce2080de9c64b322fd8a
kvelraj_concord <kvelraj@concord.net>
2023-05-13 14:38:34 +0530

    soruce code

--- learning-react/src/screens/activity.js
+++ learning-react/src/screens/activity.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 const ActivityScreen = () => {
+
+  const [bulbMode, SetBulbMode] = useState(false);
+
+  const buildONOFF = (bulbStatus) => {
+    SetBulbMode(bulbStatus);
+  }
+  
   return(
     <div>
       <h2>Check about your Account Activity</h2>
+
+      <div>
+        <button onClick={() => buildONOFF(true)}>Turn ON</button>
+        <button onClick={() => buildONOFF(false)}>Turn Off</button>
+        <div>
+          {
+            bulbMode ? 
+            <img src="https://www.w3schools.com/js/pic_bulbon.gif" />
+            :
+            <img src={require("../images/pic_bulboff.gif")} /> 
+          }
+        </div>
+      </div>
     </div>
   )
 }
