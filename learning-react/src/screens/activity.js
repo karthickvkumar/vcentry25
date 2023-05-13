459f62663b087a3f028aff7e1b73567ed3bdaa5f
kvelraj_concord <kvelraj@concord.net>
2023-05-13 15:04:52 +0530

    source code

--- learning-react/src/screens/activity.js
+++ learning-react/src/screens/activity.js
@@ -3,9 +3,14 @@ import React, {useState} from "react";
 const ActivityScreen = () => {
 
   const [bulbMode, SetBulbMode] = useState(false);
+  const [eyeMode, SetEyeMode] = useState(true);
 
   const buildONOFF = (bulbStatus) => {
     SetBulbMode(bulbStatus);
+  }
+
+  const eyeCloseOpen = (status) => {
+    SetEyeMode(status);
   }
   
   return(
@@ -13,6 +18,19 @@ const ActivityScreen = () => {
       <h2>Check about your Account Activity</h2>
 
       <div>
+        <h2>Password Visiblity</h2>
+        <div>
+          <input type={eyeMode ? "password" : "text"} placeholder="Enter your password" />
+          {
+            eyeMode ? 
+            <img src={require("../images/close-eye.png")} onClick={() => eyeCloseOpen(false)}/>
+            :
+            <img src={require("../images/open-eye.png")} onClick={() => eyeCloseOpen(true)}/>
+          }
+        </div>
+      </div>
+
+      <div className="space-top">
         <button onClick={() => buildONOFF(true)}>Turn ON</button>
         <button onClick={() => buildONOFF(false)}>Turn Off</button>
         <div>
