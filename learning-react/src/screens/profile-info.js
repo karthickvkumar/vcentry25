9e1b439ddcd4aff38f1f284c6c07feca0d7c26b9
kvelraj_concord <kvelraj@concord.net>
2023-05-14 16:39:13 +0530

    source code

--- learning-react/src/screens/profile-info.js
+++ learning-react/src/screens/profile-info.js
@@ -1,12 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
 
 const ProfileInformationScreen = () => {
 
   const [profile, UpdateProfile] = useState([]);
 
-  const loadUserProfile = () => {
-    const URL = "https://reqres.in/api/users?page=2";
+  useEffect(() => {
+    loadUserProfile(1);
+  }, []);
+
+  const loadUserProfile = (pageNo) => {
+    const URL = "https://reqres.in/api/users?page=" + pageNo;
 
     axios.get(URL)
       .then((repsonse) => {
@@ -20,27 +24,11 @@ const ProfileInformationScreen = () => {
       })
   }
 
-  const result = profile.map((value, index) => {
-    return(
-      <tr key={index}>
-        <td>{value.id}</td>
-        <td>{value.first_name}</td>
-        <td>{value.last_name}</td>
-        <td>{value.email}</td>
-        <td>
-          <img src={value.avatar} alt="user" className='avatar' />
-        </td>
-      </tr>
-    )
-  })
-
-  console.log(result)
-
   return (
     <div>
-      <h2>Update your Profile Infomration</h2>
-      <button onClick={() => loadUserProfile()}>Load Profiles</button>
-      <table>
+      {/* <h2>Update your Profile Infomration</h2> */}
+      {/* <button onClick={() => loadUserProfile()}>Load Profiles</button> */}
+      <table id="customers">
         <thead>
           <tr>
             <th>ID</th>
@@ -51,11 +39,32 @@ const ProfileInformationScreen = () => {
           </tr>
         </thead>
         <tbody>
-          
-          {result}
-          
+          {
+            profile.map((value, index) => {
+              return(
+                <tr key={index}>
+                  <td>{value.id}</td>
+                  <td>{value.first_name}</td>
+                  <td>{value.last_name}</td>
+                  <td>{value.email}</td>
+                  <td>
+                    <img src={value.avatar} alt="user" className='avatar' />
+                  </td>
+                </tr>
+              )
+            })
+          }
+          {
+            profile.length === 0 &&
+            <tr>
+              <td colSpan={5}>No Reacord Found</td>
+            </tr>
+          }
         </tbody>
       </table>
+      <button onClick={() => loadUserProfile(1)}>1</button>
+      <button onClick={() => loadUserProfile(2)}>2</button>
+      <button onClick={() => loadUserProfile(3)}>3</button>
     </div>
   );
 };
