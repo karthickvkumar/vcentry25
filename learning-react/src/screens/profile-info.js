770f752b926e3798793d2fc8753b18fead57b5d2
kvelraj_concord <kvelraj@concord.net>
2023-05-14 15:05:27 +0530

    source code

--- learning-react/src/screens/profile-info.js
+++ learning-react/src/screens/profile-info.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import axios from "axios";
 
 const ProfileInformationScreen = () => {
+
+  const loadUserProfile = () => {
+    const URL = "https://reqres.in/api/users?page=2";
+
+    axios.get(URL)
+      .then((repsonse) => {
+        const responseData = repsonse.data;
+        console.log(responseData.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   return (
     <div>
       <h2>Update your Profile Infomration</h2>
+      <button onClick={() => loadUserProfile()}>Load Profiles</button>
     </div>
   );
 };
