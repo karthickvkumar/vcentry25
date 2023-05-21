9992419252cd58080078ab1cbbd30e0a76674c6f
kvelraj_concord <kvelraj@concord.net>
2023-05-21 16:24:29 +0530

    source code

--- learning-react/src/screens/home.js
+++ learning-react/src/screens/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import HeaderComponent from "../components/header";
 import FooterComponent from "../components/footer";
@@ -11,11 +11,31 @@ const HomeScreen = () => {
 
   var userList = ["abc", "cde", "efg"];
 
+  const [form, updateForm] = useState({
+    username : ""
+  })
 
   const loadNodeAPI = () => {
     const url = "http://localhost:4000/status";
 
     axios.get(url)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  const handleInput = (event) => {
+    updateForm({...form, [event.target.name] : event.target.value });
+  }
+
+  const submitUsername = () => {
+    console.log(form);
+    const url = "http://localhost:4000/submit";
+
+    axios.post(url, form)
       .then((response) => {
         console.log(response.data);
       })
@@ -30,7 +50,14 @@ const HomeScreen = () => {
       <div className="content">
         <h1>Home Screen - {userName.email} {userList[2]}</h1>
         <img src={require("../images/shinchu.jpg")} className="image" alt=""/>
+        <br/>
         <button onClick={() => loadNodeAPI()}>Load Node API</button>
+        <div>
+          <label>Enter your Username</label>
+          <input type="text" placeholder="Enter username" onChange={handleInput} name="username" />
+          <br/>
+          <button onClick={() => submitUsername() }>Subumit Username</button>
+        </div>
       </div>
       <FooterComponent></FooterComponent>
     </div>
--- node-js/server.js
+++ node-js/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import mysql from "mysql";
 import cors from "cors";
 import http from "http";
@@ -11,10 +11,19 @@ app.use(cors({
   origin : "*"
 }));
 
+app.use(express.json());
+
 // http://localhost:4000/status
 app.get("/status", (request, response)=> {
   response.status(200).send("REST API is working fine !! Good Day");
 });
+
+// http://localhost:4000/submit
+app.post("/submit", (request, response) => {
+  const data = request.body;
+  console.log(data);
+  response.status(200).send("Value has been received from React JS");
+})
 
 
 const portNumber = 4000;
