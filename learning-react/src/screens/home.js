bb886daf3a8ac12228e01c9d5c89b9e244d1ab97
kvelraj_concord <kvelraj@concord.net>
2023-05-21 15:54:58 +0530

    source code

--- learning-react/src/screens/home.js
+++ learning-react/src/screens/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios";
 import HeaderComponent from "../components/header";
 import FooterComponent from "../components/footer";
 
@@ -11,12 +11,26 @@ const HomeScreen = () => {
 
   var userList = ["abc", "cde", "efg"];
 
+
+  const loadNodeAPI = () => {
+    const url = "http://localhost:4000/status";
+
+    axios.get(url)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   return(
     <div>
       <HeaderComponent></HeaderComponent>
       <div className="content">
         <h1>Home Screen - {userName.email} {userList[2]}</h1>
         <img src={require("../images/shinchu.jpg")} className="image" alt=""/>
+        <button onClick={() => loadNodeAPI()}>Load Node API</button>
       </div>
       <FooterComponent></FooterComponent>
     </div>
--- node-js/server.js
+++ node-js/server.js
@@ -6,9 +6,20 @@ import http from "http";
 const app = express();
 const server = http.createServer(app);
 
+app.use(cors({
+  credentials : true,
+  origin : "*"
+}));
+
+// http://localhost:4000/status
+app.get("/status", (request, response)=> {
+  response.status(200).send("REST API is working fine !! Good Day");
+});
+
+
 const portNumber = 4000;
 server.listen(portNumber, () => {
   console.log("Node JS server is running on port 4000");
-})
+});
 
 
